1988b7fa20289705bbb874ffbc560dbf04146b34
Mark Hillebrand <mahilleb@microsoft.com>
2016-10-31 15:33:28 +0100

    WIP

--- Scripts/windows/install.ps1
+++ Scripts/windows/install.ps1
@@ -68,7 +68,7 @@ Param(
     [parameter(Mandatory=$false)]
     [switch] $ForceWheelUpdate,
     [parameter(Mandatory=$false)]
-    [string] $RepoTag="v2.0.beta1.0",
+    [string] $RepoTag="v2.0.beta2.0",
     [parameter(Mandatory=$false)]
     [string] $RepoLocation="c:\repos\CNTK"
 )
